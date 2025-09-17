0d3803c20cbc09f901b548692e14395b34ae0e9f
guaika00 <zimulen@gmail.com>
2025-09-17 10:46:48 +0800

    重新添加评论url

--- packages/pure/schemas/links.ts
+++ packages/pure/schemas/links.ts
@@ -21,8 +21,8 @@ export const FriendLinksSchema = () =>
       applyTip: [
         { name: 'Name', val: 'Astro Pure' },
         { name: 'Desc', val: 'Null' },
-        { name: 'Link', val: 'https://astro-pure.js.org/' },
-        { name: 'Avatar', val: 'https://astro-pure.js.org/favicon/favicon.ico' }
+        { name: 'Link', val: 'https://guaika00.github.io/' },
+        { name: 'Avatar', val: 'https://guaika00.github.io/favicon/favicon.ico' }
       ]
     })
     .describe('Friend links for your website.')
